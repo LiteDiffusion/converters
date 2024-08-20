1c1af3fb257a07e438917270ae7022a4cf434567
zhenghaoz <zhangzhenghao@hotmail.com>
2024-08-20 20:43:14 +0800

    FIx convert ckpt

--- 0-convert_safetensors/convert_ckpt.py
+++ 0-convert_safetensors/convert_ckpt.py
@@ -1,18 +1,7 @@
 import os
-from glob import glob
 
 import click
-import torch
 from diffusers.pipelines import StableDiffusionPipeline
-from safetensors.torch import load_file
-from tqdm import tqdm
-
-
-def convert_safetensors(path: str):
-    for filename in tqdm(glob(f"{path}/*.safetensors")):
-        ckpt = load_file(filename)
-        torch.save(ckpt, filename.replace(".safetensors", ".bin"))
-        os.remove(filename)
 
 
 @click.command()
@@ -22,17 +11,10 @@ def main(path: str):
     # Create the destination directory
     dest_path = path.replace(".safetensors", "")
     os.makedirs(dest_path, exist_ok=True)
-    # Export and convert the text encoder
-    pipe.text_encoder.save_pretrained(f"{dest_path}/text_encoder")
-    convert_safetensors(f"{dest_path}/text_encoder")
-    if os.path.exists(f"{dest_path}/text_encoder/model.bin"):
-        os.rename(f"{dest_path}/text_encoder/model.bin", f"{dest_path}/text_encoder/pytorch_model.bin")
-    # Export and convert the UNet
-    pipe.unet.save_pretrained(f"{dest_path}/unet")
-    convert_safetensors(f"{dest_path}/unet")
-    # Export and convert the VAE decoder
-    pipe.vae.save_pretrained(f"{dest_path}/vae_decoder")
-    convert_safetensors(f"{dest_path}/vae_decoder")
+    # Export models
+    pipe.text_encoder.save_pretrained(f"{dest_path}/text_encoder", safe_serialization=False)
+    pipe.unet.save_pretrained(f"{dest_path}/unet", safe_serialization=False)
+    pipe.vae.save_pretrained(f"{dest_path}/vae_decoder", safe_serialization=False)
 
 
 if __name__ == '__main__':
